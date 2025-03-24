f5e0cfe211b779b68d793fe1368f353888c697a6
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-03-24 15:29:19 +0100

    added firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -9,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.groupany.mangatek"
+        applicationId = "fr.groupany.mangatek"
         minSdk = 30
         targetSdk = 35
         versionCode = 1
@@ -72,4 +73,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-}
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.google.firebase.analytics)}
