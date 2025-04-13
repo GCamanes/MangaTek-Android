5cf8aa5e6adfde9d098a10c38e7bd4ae95a6b5a3
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-04-14 01:02:28 +0200

    added exo2 font

---
+++ app/src/main/java/com/groupany/mangatek/core/theme/Exo2FontFamily.kt
@@ -0,0 +1,22 @@
+package com.groupany.mangatek.core.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.groupany.mangatek.R
+
+val exo2FontFamily = FontFamily(
+    Font(R.font.exo2_light, weight = FontWeight.Light, style = FontStyle.Normal),
+    Font(R.font.exo2_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.exo2_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+    Font(R.font.exo2_regular_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.exo2_medium, weight = FontWeight.Medium, style = FontStyle.Normal),
+    Font(R.font.exo2_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.exo2_semi_bold, weight = FontWeight.SemiBold, style = FontStyle.Normal),
+    Font(R.font.exo2_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(R.font.exo2_bold, weight = FontWeight.Bold, style = FontStyle.Normal),
+    Font(R.font.exo2_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.exo2_extra_bold, weight = FontWeight.ExtraBold, style = FontStyle.Normal),
+    Font(R.font.exo2_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+)
