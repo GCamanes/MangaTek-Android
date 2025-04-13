d756e1b5062741308fb88a1c934309f138e39fcc
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-04-14 00:13:08 +0200

    removed underline in settings screen

--- app/src/main/java/com/groupany/mangatek/features/settings/presentation/composables/SettingsElement.kt
+++ app/src/main/java/com/groupany/mangatek/features/settings/presentation/composables/SettingsElement.kt
@@ -3,7 +3,6 @@ package com.groupany.mangatek.features.settings.presentation.composables
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.style.TextDecoration
 import com.groupany.mangatek.core.presentation.composable.VerticalSpacer
 
 @Composable
@@ -13,9 +12,7 @@ fun SettingsElement(
 ) {
     Text(
         title,
-        style = MaterialTheme.typography.headlineSmall.copy(
-            textDecoration = TextDecoration.Underline
-        )
+        style = MaterialTheme.typography.headlineSmall
     )
 
     VerticalSpacer()
