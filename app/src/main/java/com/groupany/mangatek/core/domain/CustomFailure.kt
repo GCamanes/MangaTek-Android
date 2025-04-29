142f597668962069f77f18f4139d0897ef18dbb3
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-04-29 16:52:01 +0200

    added a custom failure trad extension

--- app/src/main/java/com/groupany/mangatek/core/domain/CustomFailure.kt
+++ app/src/main/java/com/groupany/mangatek/core/domain/CustomFailure.kt
@@ -1,8 +1,5 @@
 package com.groupany.mangatek.core.domain
 
-import android.content.Context
-import com.groupany.localization.R
-
 enum class FailureType (val key: String) {
      InvalidCredential("INVALID_CREDENTIALS"),
      NoUser("NO_USER"),
@@ -19,14 +16,5 @@ sealed class CustomFailure(message: String?) : Exception(message) {
 
     override fun toString(): String {
         return "${this.javaClass.kotlin.simpleName}: $message"
-    }
-
-    fun trad(context: Context): String {
-        return when(this) {
-            is InvalidCredential -> context.getString(R.string.error_credentials)
-            is NetworkError -> context.getString(R.string.error_network)
-            is TooManyRequests -> context.getString(R.string.error_too_many_request)
-            else -> context.getString(R.string.error_unknown)
-        }
     }
 }
--- app/src/main/java/com/groupany/mangatek/core/presentation/screens/ErrorScreen.kt
+++ app/src/main/java/com/groupany/mangatek/core/presentation/screens/ErrorScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.groupany.mangatek.core.domain.CustomFailure
+import com.groupany.mangatek.core.extensions.trad
 import com.groupany.ui.R
 import com.groupany.ui.components.CustomButton
 import com.groupany.ui.components.CustomSpacerSize
