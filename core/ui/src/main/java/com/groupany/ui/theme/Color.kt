8ab8ec49dc0fcc800c2e0e5fc8c04d7e746019f4
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2025-09-17 14:50:09 +0200

    fixed color background

--- core/ui/src/main/java/com/groupany/ui/theme/Color.kt
+++ core/ui/src/main/java/com/groupany/ui/theme/Color.kt
@@ -6,6 +6,6 @@ val PrimaryColor = Color(0xFF2FBBDC)  // Blue
 val SecondaryColor = Color(0xFFF57542) // Orange
 val TertiaryColor = Color(0xFF02539A)  // Dark Blue
 
-val BackgroundColor = Color(0xFF171717)
+val BackgroundColor = Color(0xFF111617)
 val SurfaceColor = Color(0xFF212121)
 val SurfaceVariantColor = Color(0xFF303030)
